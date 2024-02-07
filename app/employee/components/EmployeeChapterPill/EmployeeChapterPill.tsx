5c37ca025f5be1fc893e9afdb72b832f9e16139c
Georgios Georgiou <georgios.georgiou@newcrosshealthcare.com>
2024-02-07 15:58:08 +0200

    optimize employee chapter pill

--- app/employee/components/EmployeeChapterPill/EmployeeChapterPill.tsx
+++ app/employee/components/EmployeeChapterPill/EmployeeChapterPill.tsx
@@ -1,48 +1,51 @@
 'use client';
+import { CHAPTERS } from '@/employee/constants';
 import { Chapter } from '@/employee/types';
 
+const defaultClasses = `py-1 px-3 rounded-full text-xs`;
+
+type ChapterPillConfig = {
+  title: string;
+  class: string;
+};
+
+const chapterToClassMap: Record<Chapter, ChapterPillConfig> = {
+  web: {
+    title: 'Web',
+    class: 'bg-blue-200 text-blue-600',
+  },
+  java: {
+    title: 'Java',
+    class: 'bg-yellow-200 text-yellow-600',
+  },
+  seti: {
+    title: 'SETI',
+    class: 'bg-red-200 text-red-600',
+  },
+  '.net': {
+    title: '.NET',
+    class: 'bg-purple-200 text-purple-600',
+  },
+  'ui-ux': {
+    title: 'UI/UX',
+    class: 'bg-green-200 text-green-600',
+  },
+  data: {
+    title: 'Data',
+    class: 'bg-indigo-200 text-indigo-600',
+  },
+};
+
 export const EmployeeChapterPill = ({ chapter }: { chapter: Chapter }) => {
-  const defaultClasses = `py-1 px-3 rounded-full text-xs`;
-  switch (chapter) {
-    case 'web':
-      return (
-        <span className={`${defaultClasses} bg-blue-200 text-blue-600`}>
-          Web
-        </span>
-      );
-    case 'java':
-      return (
-        <span className={`${defaultClasses} bg-yellow-200 text-yellow-600`}>
-          Java
-        </span>
-      );
-    case 'seti':
-      return (
-        <span className={`${defaultClasses} bg-red-200 text-red-600`}>
-          SETI
-        </span>
-      );
-    case '.net':
-      return (
-        <span className={`${defaultClasses} bg-purple-200 text-purple-600`}>
-          .NET
-        </span>
-      );
-    case 'ui-ux':
-      return (
-        <span className={`${defaultClasses} bg-green-200 text-green-600`}>
-          UI/UX
-        </span>
-      );
-    case 'data':
-      return (
-        <span className={`${defaultClasses} bg-indigo-200 text-indigo-600`}>
-          Data
-        </span>
-      );
-    default: {
-      const exhaustiveCheck: never = chapter;
-      throw new Error(`Unhandled chapter: ${exhaustiveCheck}`);
-    }
+  if (!CHAPTERS.includes(chapter)) {
+    return (
+      <div className={`${defaultClasses} bg-red-400 text-red-800`}>???</div>
+    );
   }
+
+  return (
+    <div className={`${defaultClasses} ${chapterToClassMap[chapter].class}`}>
+      {chapterToClassMap[chapter].title}
+    </div>
+  );
 };
